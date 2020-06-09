584308e1ad6c934d6b74e898ee5531d0659607c7
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-06-09 12:51:12 +0100

    Fix typo in template.cpp

--- template.cpp
+++ template.cpp
@@ -19,8 +19,8 @@
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 
-typedef pair<int, int> pii
-typedef long long ll
+typedef pair<int, int> pii;
+typedef long long ll;
 
 
 using namespace std;
