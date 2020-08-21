fdb2f07c10778a7361d7fb7baf431349a3692b24
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-08-21 08:20:57 +0100

    Change file naming scheme to capital letters

--- contest.sh
+++ contest.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 #
-# Simple script which creates some files in a specified directory using the 
+# Simple script which creates some files in a specified directory using the
 # `template.cpp` file.
 #
 # It can be run with:
-#     ./contest <dir> <num>
+#     ./contest <directory> <numOfFiles>
 
 mkdir -p "$1"
 
 template=$(<template.cpp)
 
-for (( i=1; i <= "$2"; i++ )) do
-    echo "$template" >"$1/$i.cpp"
+chr() {
+  [ "$1" -lt 256 ] || return 1
+  printf "\\$(printf %o "$1")"
+}
+
+for ((i = 0; i < "$2"; i++)); do
+  echo "$template" >"$1/$(chr $((i + 65))).cpp"
 done
